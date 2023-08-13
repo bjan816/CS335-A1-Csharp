b6dad9b96fe2eb0cb79042edd2c79a7b29ca2ee9
Kellie-J <bjan816@aucklanduni.ac.nz>
2023-08-14 06:21:50 +1200

    Add Endpoint 1: GET THE VERSION OF THE WEB API (1 mark)

--- Controllers/A1Controller.cs
+++ Controllers/A1Controller.cs
@@ -1,1 +1,18 @@
-﻿
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace A1.Controllers
+{
+    [Route("webapi")]
+    [ApiController]
+    public class A1Controller : Controller
+    {
+        // GET /webapi/GetVersion
+        [HttpGet("GetVersion")]
+        public IActionResult GetVersion()
+        {
+            const string upi = "bjan816";
+            const string versionString = $"1.0.0 (Ngongotahā) by {upi}";
+            return Ok(versionString);
+        }
+    }
+}
