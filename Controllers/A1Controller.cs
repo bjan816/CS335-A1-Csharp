8ed67266e5c486bfd1d09aa59c0dd491d8aecb12
Kellie-J <bjan816@aucklanduni.ac.nz>
2023-08-14 06:31:09 +1200

    Add Endpoint 2: GET THE LOGO OF THE COMPANY (1 mark)

--- Controllers/A1Controller.cs
+++ Controllers/A1Controller.cs
@@ -14,5 +14,48 @@ namespace A1.Controllers
             const string versionString = $"1.0.0 (Ngongotahā) by {upi}";
             return Ok(versionString);
         }
+
+        private string GetContentType(string fileName)
+        {
+            switch (Path.GetExtension(fileName).ToLower())
+            {
+                case ".png":
+                {
+                    return "image/png";
+                }
+                case ".jpg":
+                case ".jpeg":
+                {
+                    return "image/jpeg";
+                }
+                case ".gif":
+                {
+                    return "image/gif";
+                }
+                default:
+                {
+                    return "application/octet-stream";
+                }
+            }
+        }
+
+        // GET /webapi/GetLogo
+        [HttpGet("GetLogo")]
+        public IActionResult GetLogo()
+        {
+            string currentDirectory = Directory.GetCurrentDirectory();
+            string logoImageFilePath = Path.Combine(currentDirectory, "Logos/Logo.png");
+
+            if (!System.IO.File.Exists(logoImageFilePath))
+            {
+                return NotFound();
+            }
+
+            byte[] imageBytes = System.IO.File.ReadAllBytes(logoImageFilePath);
+
+            string contentType = GetContentType(logoImageFilePath);
+
+            return File(imageBytes, contentType);
+        }
     }
 }
